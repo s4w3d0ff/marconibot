65de5d3216f959d12ed64082661122b35c58c7f3
s4w3d0ff <usmcfericano@gmail.com>
2017-06-16 13:13:43 -0700

    add graph method

--- marconi/chart.py
+++ marconi/chart.py
@@ -1,6 +1,7 @@
 from tools import time, getMongoDb, logging, itemgetter
-from tools import pd, np
+from tools import pd, np, figure, output_file, PI
 from tools.indicators import ema, macd, bbands, rsi
+from bokeh.models import LinearAxis, Range1d
 logger = logging.getLogger(__name__)
 
 
@@ -58,19 +59,20 @@ class Chart(object):
         return sorted(list(self.db.find()), key=itemgetter('_id'))[-size:]
 
     def dataFrame(self, size=0, window=120):
+        """ returns pandas DataFrame from raw db data with indicators"""
         # get data from db
         data = self.__call__(size)
         # make dataframe
         df = pd.DataFrame(data)
-        print(df.tail())
         df['date'] = pd.to_datetime(df["_id"], unit='s')
         df.set_index('_id', inplace=True)
         # calculate/add sma and bbands
         df = bbands(df, window)
         # add slow ema
-        df = ema(df, window // 2, colname='emaslow')
+        slowWin = window // 2
+        df = ema(df, slowWin, colname='emaslow')
         # add fast ema
-        df = ema(df, window // 4, colname='emafast')
+        df = ema(df, slowWin // 4, colname='emafast')
         # add macd
         df = macd(df)
         # add rsi
@@ -81,43 +83,96 @@ class Chart(object):
         df['percentChange'] = df['close'].pct_change()
         return df
 
+    def graph(self, size=0, window=120):
+        df = self.dataFrame(size, window)
+        df.dropna(inplace=True)
+
+        output_file("%s.html" % self.pair,
+                    title="%s-Poloniex" % self.pair)
+
+        p = figure(x_axis_type="datetime",
+                   y_range=(min(df['low'].values),
+                            max(df['high'].values)),
+                   tools="pan,wheel_zoom,box_zoom,reset,save",
+                   plot_width=1500,
+                   title=self.pair)
+
+        p.extra_y_ranges = {
+            "volume": Range1d(start=1,
+                              end=max(df['volume'].values))
+        }
+
+        candleWidth = (self.period * 1000) - self.period * 100
+
+        p.xaxis.major_label_orientation = PI / 4
+        p.grid.grid_line_alpha = 0.8
+
+        # Plot volume
+        p.vbar(x=df['date'],
+               width=candleWidth,
+               top=df['volume'],
+               bottom=0,
+               fill_color="blue",
+               alpha=0.2,
+               y_range_name="volume")
+
+        #p.add_layout(LinearAxis(y_range_name="volume"), 'left')
+
+        # Plot candle 'shadows'/wicks
+        p.segment(x0=df.date,
+                  y0=df.high,
+                  x1=df.date,
+                  y1=df.low,
+                  color="black",
+                  line_width=1)
+
+        # Plot green candles
+        inc = df.close > df.open
+        p.vbar(x=df.date[inc],
+               width=candleWidth,
+               top=df.open[inc],
+               bottom=df.close[inc],
+               fill_color="green",
+               line_color="black"
+               )
+
+        # Plot red candles
+        dec = df.open > df.close
+        p.vbar(x=df.date[dec],
+               width=candleWidth,
+               top=df.open[dec],
+               bottom=df.close[dec],
+               fill_color="red",
+               line_color="black")
+
+        # Plot bbands
+        band_x = np.append(df['date'].values, df['date'].values[::-1])
+        band_y = np.append(df['bbbottom'].values, df['bbtop'].values[::-1])
+        p.patch(band_x,
+                band_y,
+                color='navy',
+                fill_alpha=0.1)
+
+        # Plot moving averages
+        p.line(df['date'], df['sma'], color='navy',
+               alpha=0.9)
+        p.line(df['date'], df['emaslow'], color='orange',
+               alpha=0.9)
+        p.line(df['date'], df['emafast'], color='red',
+               alpha=0.9)
+
+        return p
+
 if __name__ == '__main__':
     from tools.poloniex import Poloniex
-    from tools import PI, figure, output_file, show
+    from tools import show
 
     logging.basicConfig(level=logging.DEBUG)
     logging.getLogger("tools.poloniex").setLevel(logging.INFO)
     logging.getLogger('requests').setLevel(logging.ERROR)
 
-    period = 60 * 60 * 4
-    w = (period * 1000) - 5000
-    TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
-    output_file("BTC_ETH.html", title="BTC_ETH-Poloniex")
+    period = 60 * 60 * 24
 
     api = Poloniex(jsonNums=float)
-    df = Chart(api, 'BTC_ETH', period=period).dataFrame(250, window=80)
-
-    df.dropna(inplace=True)
-    print(df.tail())
-
-    inc = df.close > df.open
-    dec = df.open > df.close
-
-    p = figure(x_axis_type="datetime", tools=TOOLS,
-               plot_width=1500, title="BTC_ETH")
-
-    p.xaxis.major_label_orientation = PI / 4
-    p.grid.grid_line_alpha = 0.7
-
-    p.segment(df.date, df.high, df.date, df.low, color="black")
-
-    p.vbar(df.date[inc], w, df.open[inc], df.close[inc],
-           fill_color="green", line_color="black")
-    p.vbar(df.date[dec], w, df.open[dec], df.close[dec],
-           fill_color="red", line_color="black")
-
-    p.line(df['date'], df['sma'], color='navy', alpha=0.5)
-    p.line(df['date'], df['bbtop'], color='red', alpha=0.9)
-    p.line(df['date'], df['bbbottom'], color='red', alpha=0.9)
-
+    p = Chart(api, 'BTC_ETH', period=period).graph(window=120)
     show(p)
