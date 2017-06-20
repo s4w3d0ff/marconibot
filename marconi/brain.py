14f79f10bbbd6cbe803c74efce9b5c435443431d
s4w3d0ff <usmcfericano@gmail.com>
2017-06-20 12:02:47 -0700

    cleanup brain and enhance chart

--- marconi/brain.py
+++ marconi/brain.py
@@ -6,58 +6,82 @@ from tools import getMongoDb, logging, pd, np, time
 logger = logging.getLogger(__name__)
 
 
-def autoLabels(df, target='close'):
-    # create future
-    df['future'] = df['close'].shift(-1)
-    df.fillna(0, inplace=True)
-    # label sells
-    df.loc[df['future'] < df['close'], 'label'] = -1
-    # label buys
-    df.loc[df['future'] > df['close'], 'label'] = 1
-    # label holds
-    df.loc[df['future'] == df['close'], 'label'] = 0
-    df.fillna(0, inplace=True)
-    return df
-
-
 class Brain(object):
 
-    def __init__(self):
-        self.lobe = svm.SVC(gamma=0.001, C=100.)
+    def __init__(self, lobe=False):
+        if isinstance(lobe, str):
+            self.lobe = pickle.load(open(lobe, 'rb'))
+        else:
+            self.lobe = svm.SVC(gamma=0.001, C=100.)
+
+    def pickle(self, name='lobe.pickle'):
+        pickle.dump(self.lobe, open(name, wb))
+
+    def splitData(self, df, size=1):
+        # make infinity nan
+        df.replace([np.inf, -np.inf], np.nan, inplace=True)
+        # make nan the mean
+        df.fillna(df.mean(), inplace=True)
+        # split db
+        return df.tail(size), df.iloc[:-size]
+
+    def getLabels(self, df, target='close'):
+        # create future
+        df['future'] = df[target].shift(-1)
+        df.fillna(df.mean(), inplace=True)
+        # label sells
+        df.loc[df['future'] < df[target], 'label'] = -1
+        # label buys
+        df.loc[df['future'] > df[target], 'label'] = 1
+        # label holds
+        df.loc[df['future'] == df[target], 'label'] = 0
+        df.fillna(df.mean(), inplace=True)
+        return df
+
+    def train(self, f, l):
+        self.lobe.fit(f, l)
 
 
 if __name__ == '__main__':
-    from chart import Chart
+    from chart import Charter
     from poloniex import Poloniex
     logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('tools.poloniex').setLevel(logging.INFO)
-    market = 'BTC_LTC'
+    logging.getLogger('poloniex').setLevel(logging.INFO)
+    markets = ['BTC_LTC',
+               'BTC_ETH',
+               'USDT_BTC',
+               'USDT_LTC',
+               'USDT_ETH',
+               'BTC_DASH',
+               'USDT_DASH']
     brain = Brain()
-    df = Chart(Poloniex(jsonNums=float),
-               pair=market,
-               period=60 * 60 * 4).dataFrame(window=120)
-    df.replace([np.inf, -np.inf], np.nan, inplace=True)
-    df.fillna(0, inplace=True)
+    charter = Charter(Poloniex(jsonNums=float),
+                      pair=markets[0],
+                      period=60 * 60 * 4)
+    testDFs = {}
+    for market in markets:
 
-    testDf, trainDf = df.tail(1), df.iloc[:-1]
-    print(testDf)
-    print(trainDf)
+        df = charter.dataFrame(market, window=120)
+        testDFs[market], train = brain.splitData(df, size=2)
 
-    trainDf = autoLabels(trainDf)
-    labels = trainDf['label'].values
+        train = brain.getLabels(train)
 
-    features = trainDf[['bbpercent',
-                        'bodysize',
-                        'macd',
-                        'bbrange',
-                        'rsi']].values
+        labels = train['label'].values
+        features = train[['bbpercent',
+                          'bodysize',
+                          'macd',
+                          'bbrange',
+                          'rsi',
+                          'percentChange']].values
 
-    logger.info("Training size: %s", str(len(features)))
+        logger.info("%s training size: %s", market, str(len(features)))
+        brain.train(features, labels)
 
-    brain.lobe.fit(features, labels)
-
-    print(brain.lobe.predict(testDf[['bbpercent',
-                                     'bodysize',
-                                     'macd',
-                                     'bbrange',
-                                     'rsi']].values))
+    for market in testDFs:
+        print(market)
+        print(brain.lobe.predict(testDFs[market][['bbpercent',
+                                                  'bodysize',
+                                                  'macd',
+                                                  'bbrange',
+                                                  'rsi',
+                                                  'percentChange']].values))
